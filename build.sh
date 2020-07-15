dd9542bc677ac8dc1272f99438e1f2246c657993
ivan-volnov <51086293+ivan-volnov@users.noreply.github.com>
2020-07-15 21:48:23 +0800

    Build compiler-rt builtins

--- build.sh
+++ build.sh
@@ -24,42 +24,9 @@ export STRIP=llvm-strip
 
 
 
-mkdir $root_dir/tmp
+mkdir -p $root_dir/tmp
 # rm -fr $root_dir/tmp/*
 cd $root_dir/tmp
-
-# llvm_libs='compiler-rt libcxx libcxxabi libunwind'
-# for llvm_lib in $llvm_libs; do
-#     wget https://github.com/llvm/llvm-project/releases/download/llvmorg-$llvm_version/$llvm_lib-$llvm_version.src.tar.xz || exit 1
-#     tar xf $llvm_lib-$llvm_version.src.tar.xz || exit 1
-#     rm $llvm_lib-$llvm_version.src.tar.xz || exit 1
-# done
-
-
-# cmake \
-#     -DLIBCXXABI_LIBCXX_PATH=$ROOT/src/libcxx \
-#     -DLIBCXXABI_LIBCXX_INCLUDES=$ROOT/src/libcxx/include \
-#     -DLIBCXXABI_ENABLE_STATIC_UNWINDER=ON \
-#     -DLIBCXXABI_USE_LLVM_UNWINDER=ON \
-#     -DLIBCXX_LIBCXXABI_INCLUDES_INTERNAL=$ROOT/src/libcxxabi/include \
-#     -DLIBCXX_HAS_MUSL_LIBC=ON \
-#     -DLIBCXX_HAS_GCC_S_LIB=OFF \
-#     -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON \
-#     -DLIBUNWIND_ENABLE_SHARED=OFF \
-#     -DCLANG_DEFAULT_CXX_STDLIB=libc++ \
-#     -DCLANG_DEFAULT_LINKER=lld \
-#     -DCLANG_DEFAULT_RTLIB=compiler-rt \
-#     -DLLVM_DEFAULT_TARGET_TRIPLE="$ARCH"-pc-linux-musl \
-#     -DDEFAULT_SYSROOT=/"$ARCH"-pc-linux-musl \
-#     -DCMAKE_INSTALL_PREFIX=/"$ARCH"-pc-linux-musl \
-#     -DCMAKE_BUILD_TYPE=Release \
-#     -DLLVM_TARGET_ARCH="$ARCH" \
-#     -DLLVM_TARGETS_TO_BUILD="$TARGETS" \
-#     -G Ninja \
-#     $ROOT/src/llvm || exit 40
-#
-# cmake -G Ninja
-# ninja
 
 # musl
 wget http://musl.libc.org/releases/musl-$musl_version.tar.gz || exit 1
@@ -67,7 +34,47 @@ tar xfz musl-$musl_version.tar.gz || exit 1
 cd musl-$musl_version
 ./configure --disable-shared --enable-debug --prefix=$root_dir
 make -j5 install
-
-
 cd ..
-rm -fr $root_dir/tmp
+
+llvm_libs='compiler-rt libcxx libcxxabi libunwind'
+for llvm_lib in $llvm_libs; do
+    wget https://github.com/llvm/llvm-project/releases/download/llvmorg-$llvm_version/$llvm_lib-$llvm_version.src.tar.xz || exit 1
+    tar xf $llvm_lib-$llvm_version.src.tar.xz || exit 1
+    rm $llvm_lib-$llvm_version.src.tar.xz || exit 1
+done
+
+
+cd compiler-rt-$llvm_version.src
+
+cmake \
+    -DCMAKE_TOOLCHAIN_FILE=$root_dir/toolchain.cmake \
+    -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -DCOMPILER_RT_DEFAULT_TARGET_TRIPLE=$target \
+    -DCOMPILER_RT_BUILD_BUILTINS=ON \
+    -DCOMPILER_RT_BUILD_SANITIZERS=OFF \
+    -DCOMPILER_RT_BUILD_XRAY=OFF \
+    -DCOMPILER_RT_BUILD_LIBFUZZER=OFF \
+    -DCOMPILER_RT_BUILD_CRT=OFF \
+    -DCOMPILER_RT_USE_BUILTINS_LIBRARY=ON \
+    -DLIBCXXABI_LIBCXX_PATH=$root_dir/tmp/libcxx-$llvm_version.src \
+    -DLIBCXXABI_LIBCXX_INCLUDES=$root_dir/tmp/libcxx-$llvm_version.src/include \
+    -DLIBCXXABI_ENABLE_STATIC_UNWINDER=ON \
+    -DLIBCXXABI_USE_LLVM_UNWINDER=ON \
+    -DLIBCXX_LIBCXXABI_INCLUDES_INTERNAL=$root_dir/tmp/libcxxabi-$llvm_version.src/include \
+    -DLIBCXX_HAS_MUSL_LIBC=ON \
+    -DLIBCXX_HAS_GCC_S_LIB=OFF \
+    -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON \
+    -DLIBUNWIND_ENABLE_SHARED=OFF \
+    -DCLANG_DEFAULT_CXX_STDLIB=libc++ \
+    -DCLANG_DEFAULT_LINKER=lld \
+    -DCLANG_DEFAULT_RTLIB=compiler-rt \
+    -DCMAKE_INSTALL_PREFIX=$root_dir \
+    -DCMAKE_BUILD_TYPE=Release \
+    . || exit 1
+
+make install -j8
+
+
+# cd ..
+# rm -fr $root_dir/tmp
